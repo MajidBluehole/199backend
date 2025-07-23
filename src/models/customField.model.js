29f6ca0bda90192565cad59e9b1da6869bcc1309
majidbluehole <majid.vainavins@gmail.com>
2025-07-23 10:40:01 +0530

    id datatype changed

--- src/models/customField.model.js
+++ src/models/customField.model.js
@@ -10,9 +10,10 @@ const FIELD_TYPES = {
 module.exports = (sequelize, DataTypes) => {
   const CustomField = sequelize.define('CustomField', {
      id: {
-     type: DataTypes.BIGINT.UNSIGNED,
-     autoIncrement: true,
-     primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
     },
     name: {
       type: DataTypes.STRING(255),
